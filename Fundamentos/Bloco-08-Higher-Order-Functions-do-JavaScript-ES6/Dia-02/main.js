12fc48dcbe8d958f22cf30f10ebf7611c22878ff
Rafael Toledo <rafael.toledo@engenharia.ufjf.br>
2021-09-09 18:11:34 -0300

    .

---
+++ Fundamentos/Bloco-08-Higher-Order-Functions-do-JavaScript-ES6/Dia-02/main.js
@@ -0,0 +1,10 @@
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => 
+  // Adiciona seu código aqui
+  numbers.find((number) => number % 3 === 0 && number % 5 === 0)
+
+
+
+console.log(findDivisibleBy3And5())
