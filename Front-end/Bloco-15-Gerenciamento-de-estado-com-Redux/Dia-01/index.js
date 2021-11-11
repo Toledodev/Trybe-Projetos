1a56833556fde1e3a807845a5d8f1e874ed1529f
Toledodev <rafael.toledo@engenharia.ufjf.br>
2021-11-11 18:14:06 -0300

    .

--- Front-end/Bloco-15-Gerenciamento-de-estado-com-Redux/Dia-01/index.js
+++ Front-end/Bloco-15-Gerenciamento-de-estado-com-Redux/Dia-01/index.js
@@ -1,32 +1,56 @@
-const Redux = require('redux');
-const store = Redux.createStore();
+// const Redux = require('redux');
+// const store = Redux.createStore();
 
-const fazerLogin = (email) => ({
-    type: "LOGIN",
-    email});
+// const fazerLogin = (email) => ({
+//     type: "LOGIN",
+//     email});
   
-  const ESTADO_INICIAL = {
-    login: false,
-    email: "",
-  };
+//   const ESTADO_INICIAL = {
+//     login: false,
+//     email: "",
+//   };
   
-  const reducer = (state = ESTADO_INICIAL, action) => {
-    switch (action.type) {
-      case "LOGIN":
-        return {
-          ...state,
-          login: !state.login,
-          email: action.email,
-        };
-      default: // No switch, sempre precisamos ter um caso default!
-        return state;
-    }
-  };
+//   const reducer = (state = ESTADO_INICIAL, action) => {
+//     switch (action.type) {
+//       case "LOGIN":
+//         return {
+//           ...state,
+//           login: !state.login,
+//           email: action.email,
+//         };
+//       default: // No switch, sempre precisamos ter um caso default!
+//         return state;
+//     }
+//   };
   
-  const store = Redux.createStore(reducer);
+//   const store = Redux.createStore(reducer);
   
-  store.dispatch(fazerLogin("alguem@email.com"));
+//   store.dispatch(fazerLogin("alguem@email.com"));
   
-  console.log(store.getState());
+//   console.log(store.getState());
   
-  // { login: true, email: 'alguem@email.com' }
+//   // { login: true, email: 'alguem@email.com' }
+// Arquivo index.js
+
+import { combineReducers } from 'redux';
+import meuReducer from './meuReducer';
+import meuOutroReducer from './meuOutroReducer';
+
+const reducerCombinado = combineReducers({
+meuReducer,
+meuOutroReducer});
+
+export default reducerCombinado;
+
+import { createStore } from 'redux';
+// Importando o reducer combinado que fizemos logo acima
+import reducerCombinado from './reducers/index';
+
+const store = createStore(reducerCombinado);
+
+console.log(store.getState())
+
+//{
+// meuReducer: {/_estado do meuReducer_/},
+// meuOutroReducer: {/_estado do meuOutroReducer_/,}
+//}
